a51bb312e56ca2d23e4b747ecf5b81eca29c2107
Solomon Kebede <solomonaau188@gmail.com>
2023-01-05 17:04:10 +0000

    Task 9 - Add function docstrings

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -13,4 +13,6 @@ import typing
 def element_length(
         lst: typing.Iterable[typing.Sequence]
         ) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    '''Return a list of tuple containing the element
+    and the its length'''
     return [(i, len(i)) for i in lst]
