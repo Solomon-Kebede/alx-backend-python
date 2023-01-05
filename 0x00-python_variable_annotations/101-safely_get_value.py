f9a700042cbcbbcf06e5415061256df3dd454f92
Solomon Kebede <solomonaau188@gmail.com>
2023-01-05 20:48:15 +0000

    Task 11. More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,24 @@
-def safely_get_value(dct, key, default = None):
+#!/usr/bin/env python3
+
+'''
+Given the parameters and the return values, add
+type annotations to the function
+*Hint: look into TypeVar*
+'''
+
+
+import typing
+from typing import TypeVar, Optional
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: typing.Mapping,
+        key: typing.Any,
+        default: typing.Union[Optional[T], None] = None
+        ) -> typing.Union[typing.Any, Optional[T]]:
+    '''Safely retrieve value from dict
+    using key'''
     if key in dct:
         return dct[key]
     else:
