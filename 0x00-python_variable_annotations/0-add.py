ab2a65ac54b99cf5feece4776e4ed3bd1be7f767
Solomon Kebede <solomonaau188@gmail.com>
2023-01-05 14:46:29 +0000

    Task 0 - Pycodestyle fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,10 +2,10 @@
 
 '''
 Write a type-annotated function add that takes a float `a`
-and a float `b` as arguments and returns their sum as a float. 
+and a float `b` as arguments and returns their sum as a float.
 '''
 
 
 def add(a: float, b: float) -> float:
-	'Adds two float integers'
-	return a + b
+    '''Adds two float integers'''
+    return a + b
