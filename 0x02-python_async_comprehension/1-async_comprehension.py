35ea284f50d7b22bd168b66ae24707401d8ebab1
Solomon Kebede <solomonaau188@gmail.com>
2023-01-11 02:48:54 +0000

    Task 1 - Import fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,6 +10,9 @@ an async comprehensing over async_generator, then
 return the 10 random numbers.
 '''
 
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension():
     '''Async Comprehension'''
