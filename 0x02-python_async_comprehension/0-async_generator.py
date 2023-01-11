e4aaf97f8d258a300eed79c7ae08ca5f21ed9169
Solomon Kebede <solomonaau188@gmail.com>
2023-01-11 03:01:47 +0000

    Task 0 and 1 pycode fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,6 +14,7 @@ import asyncio
 import random
 import typing
 
+
 async def async_generator() -> typing.Generator[float, None, None]:
     '''Async Generator'''
     for i in range(10):
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,8 +11,9 @@ return the 10 random numbers.
 '''
 
 import asyncio
-async_generator = __import__('0-async_generator').async_generator
 import typing
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> typing.List[float]:
     '''Async Comprehension'''
