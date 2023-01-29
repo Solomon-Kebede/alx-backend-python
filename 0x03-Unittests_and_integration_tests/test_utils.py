885c40496da436928cacfaa02b56be58ad403ff8
Solomon Kebede <solomonaau188@gmail.com>
2023-01-29 11:59:34 +0000

    Task 1. Parameterize a unit test: test+docs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,6 +23,19 @@ class TestAccessNestedMap(unittest.TestCase):
             expected_result = expected_result.get(path[i])
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        '''Testcase for Exceptions in utils.access_nested_map'''
+        expected_result = nested_map
+        for i in range(len(path)):
+            key = path[i]
+            if key not in expected_result.keys():
+                with self.assertRaises(KeyError):
+                    expected_result = expected_result[key]
+
 
 if __name__ == '__main__':
     unittest.main()
