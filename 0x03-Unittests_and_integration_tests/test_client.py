daf5a8381d717b004b221711f30f4a4bafb501c1
Solomon Kebede <solomonaau188@gmail.com>
2023-01-30 21:02:45 +0000

    Task 6 - reorganize 2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -58,15 +58,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ),
     ])
     @patch('client.get_json')
-    def test_public_repos(self, org_name,
-        json_response, repos_list, mock_get_json
-    ):
+    def test_public_repos(self, org_name, json_res, repos_list, mock_get_json):
         '''Testcase for client.GithubOrgClient.public_repos'''
-        mock_get_json.return_value = json_response # patch get_json
-        with patch('client.GithubOrgClient._public_repos_url',
+        mock_get_json.return_value = json_res  # patch get_json
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
             new_callable=PropertyMock
         ) as mock_public_repos_url:
-            mock_public_repos_url.return_value = json_response
+            mock_public_repos_url.return_value = json_res
             instance = GithubOrgClient(org_name)
             '''Test that the repo list is what you expect
             from the chosen payload'''
@@ -74,7 +73,6 @@ class TestGithubOrgClient(unittest.TestCase):
         # Test that the mocked property and the mocked get_json was called once
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
-
 
 
 if __name__ == '__main__':
